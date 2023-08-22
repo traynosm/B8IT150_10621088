92c3815ce849efdc8727d0655ee015716624882d
Susan Traynor <traynosm@gmail.com>
2023-08-22 20:27:37 +0100

    Put and post functionality
    put and post for products made functional

--- src/core-strength-yoga-products-management-api/core-strength-yoga-products-management-api/Controllers/ProductsController.cs
+++ src/core-strength-yoga-products-management-api/core-strength-yoga-products-management-api/Controllers/ProductsController.cs
@@ -100,50 +100,6 @@ namespace core_strength_yoga_products_api.Controllers
             return products.Any() ? products.ToList() : new List<Product>();
         }
 
-        [Microsoft.AspNetCore.Mvc.HttpPut()]
-        public async Task<ActionResult<Product>> Put(Product productToUpdate)
-        {
-            if(productToUpdate.Id == 0)
-            {
-                return NotFound();
-            }
-            
-            var savedProduct = _context.Products
-                .IncludeAllRelated()
-                .FirstOrDefault(p => p.Id == productToUpdate.Id);
-
-            if(savedProduct != null)
-            {
-                RedirectToAction("Post", new { productToUpdate });
-            }
-
-            UpdateProductAttributes(productToUpdate, savedProduct!); 
-            UpdateProductCategory(productToUpdate, savedProduct!);
-            UpdateProductType(productToUpdate, savedProduct!);   
-            UpdateImage(productToUpdate, savedProduct!);
-
-            _context.Entry(savedProduct!).CurrentValues.SetValues(productToUpdate);
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!_context.Products.Any(p => p.Id == productToUpdate.Id))
-                {
-                    return NotFound();
-                }
-            }
-            catch(Exception exception)
-            {
-                return StatusCode(500, exception.Message);
-            }
-
-            
-            return productToUpdate;
-        }
-
         [Microsoft.AspNetCore.Authorization.Authorize]
         [Microsoft.AspNetCore.Mvc.HttpPost]
         public async Task<ActionResult<Product>> Post(Product product)
@@ -158,14 +114,14 @@ namespace core_strength_yoga_products_api.Controllers
                 return Problem($"Product with name='{product.Name}' already exists!");
             }
 
-            if (product.ProductTypeId > 0)
+            if (product.ProductType.Id > 0)
             {
                 _context.ProductTypes.Attach(product.ProductType);
             }
 
-            if (product.ProductCategoryId > 0)
+            if (product.ProductCategory.Id > 0)
             {
-                _context.ProductCategories.Attach(product.ProductCategory);
+               _context.ProductCategories.Attach(product.ProductCategory);
             }
 
             if (product.ImageId > 0)
@@ -186,13 +142,75 @@ namespace core_strength_yoga_products_api.Controllers
                 {
                     _context.ProductAttributes.Attach(productAttribute);
                 }
+                else
+                {
+                    _context.Add(productAttribute);
+                }
             }
 
             _context.Products.Add(product);
 
-            await _context.SaveChangesAsync();
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(p => p.Id == product.Id))
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
 
             return RedirectToAction($"Get", new { product.Id });
+        }
+
+        [Microsoft.AspNetCore.Authorization.Authorize]
+        [Microsoft.AspNetCore.Mvc.HttpPut]
+        public async Task<ActionResult<Product>> Put(Product productToUpdate)
+        {
+            if (productToUpdate.Id == 0)
+            {
+                return NotFound();
+            }
+
+            var savedProduct = _context.Products
+                .IncludeAllRelated()
+                .FirstOrDefault(p => p.Id == productToUpdate.Id);
+
+            if (savedProduct != null)
+            {
+                RedirectToAction("Post", new { productToUpdate });
+            }
+
+            UpdateProductAttributes(productToUpdate, savedProduct!);
+            productToUpdate = UpdateProductCategory(productToUpdate, savedProduct!);
+            productToUpdate = UpdateProductType(productToUpdate, savedProduct!);
+            productToUpdate = UpdateImage(productToUpdate, savedProduct!);
+
+            _context.Entry(savedProduct!).CurrentValues.SetValues(productToUpdate);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(p => p.Id == productToUpdate.Id))
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return productToUpdate;
         }
 
         [Microsoft.AspNetCore.Authorization.Authorize]
@@ -219,6 +237,11 @@ namespace core_strength_yoga_products_api.Controllers
         {
             foreach (var productAttributeToUpdate in productToUpdate.ProductAttributes)
             {
+                if(productAttributeToUpdate.ProductId == 0)
+                {
+                    productAttributeToUpdate.ProductId = savedProduct.Id;
+                }
+                
                 var savedProductAttribute = savedProduct.ProductAttributes.SingleOrDefault(
                     pa => pa.Id == productAttributeToUpdate.Id);
 
@@ -233,22 +256,85 @@ namespace core_strength_yoga_products_api.Controllers
             }
         }
 
-        private void UpdateProductCategory(Product productToUpdate, Product savedProduct)
+        private Product UpdateProductCategory(Product productToUpdate, Product savedProduct)
         {
-             _context.Entry(savedProduct.ProductCategory).CurrentValues
-                .SetValues(productToUpdate.ProductCategory);       
+            if(productToUpdate.ProductCategory.Id == savedProduct.ProductCategory.Id)
+            {
+                productToUpdate.ProductCategory = savedProduct.ProductCategory;
+                productToUpdate.ProductCategoryId = savedProduct.ProductCategoryId;
+
+                return productToUpdate;
+            }
+            else
+            {
+                var updatedProductCategory = _context.ProductCategories.Find(
+                    productToUpdate.ProductCategory.Id) ?? 
+                    throw new NullReferenceException(
+                        $"Could not find ProductCategory with Id=" +
+                        $"{productToUpdate.ProductCategory.Id}");
+
+                productToUpdate.ProductCategory = updatedProductCategory;
+                productToUpdate.ProductCategoryId = updatedProductCategory.Id;
+            }
+
+             //_context.Entry(savedProduct.ProductCategory).CurrentValues
+             //   .SetValues(productToUpdate.ProductCategory);
+            
+            return productToUpdate;
         }
 
-        private void UpdateProductType(Product productToUpdate, Product savedProduct)
+        private Product UpdateProductType(Product productToUpdate, Product savedProduct)
         {
-            _context.Entry(savedProduct.ProductType).CurrentValues
-               .SetValues(productToUpdate.ProductType);
+            if (productToUpdate.ProductType.Id == savedProduct.ProductType.Id)
+            {
+                productToUpdate.ProductType = savedProduct.ProductType;
+                productToUpdate.ProductTypeId = savedProduct.ProductTypeId;
+
+                return productToUpdate;
+            }
+            else
+            {
+                var updatedProductType = _context.ProductTypes.Find(
+                    productToUpdate.ProductType.Id) ??
+                    throw new NullReferenceException(
+                        $"Could not find ProductType with Id=" +
+                        $"{productToUpdate.ProductType.Id}");
+
+                productToUpdate.ProductType = updatedProductType;
+                productToUpdate.ProductTypeId = updatedProductType.Id;
+            }
+
+            //_context.Entry(savedProduct.ProductType).CurrentValues
+            //   .SetValues(productToUpdate.ProductType);
+
+            return productToUpdate;
         }
 
-        private void UpdateImage(Product productToUpdate, Product savedProduct)
+        private Product UpdateImage(Product productToUpdate, Product savedProduct)
         {
+            if (productToUpdate.Image.Id == savedProduct.Image.Id)
+            {
+                productToUpdate.Image = savedProduct.Image;
+                productToUpdate.ImageId = savedProduct.ImageId;
+
+                return productToUpdate;
+            }
+            else
+            {
+                var updatedImage = _context.Images.Find(
+                    productToUpdate.Image.Id) ??
+                    throw new NullReferenceException(
+                        $"Could not find Image with Id=" +
+                        $"{productToUpdate.Image.Id}");
+
+                productToUpdate.Image = updatedImage;
+                productToUpdate.ImageId = updatedImage.Id;
+            }
+
             _context.Entry(savedProduct.Image).CurrentValues
                .SetValues(productToUpdate.Image);
+
+            return productToUpdate;
         }
     }
 }
---
+++ src/core-strength-yoga-products-management-api/core-strength-yoga-products-management-api/Migrations/20230822125019_Nullables.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace core_strength_yoga_products_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nullables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ProductTypes",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ProductCategories",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Path",
+                table: "Images",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageName",
+                table: "Images",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Alt",
+                table: "Images",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ProductTypes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "ProductCategories",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Path",
+                table: "Images",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageName",
+                table: "Images",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Alt",
+                table: "Images",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
