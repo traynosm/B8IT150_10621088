8ff71d0112e71403da6240eb72d9906fa8ebadfb
Susan Traynor <traynosm@gmail.com>
2023-09-01 21:33:43 +0100

    Filter updated
    updated to filter on relevant areas

--- src/core-strength-yoga-products-management-api/core-strength-yoga-products-management-api/Controllers/StockAuditController.cs
+++ src/core-strength-yoga-products-management-api/core-strength-yoga-products-management-api/Controllers/StockAuditController.cs
@@ -5,6 +5,7 @@ using core_strength_yoga_products_api.Model;
 using core_strength_yoga_products_api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Eventing.Reader;
 using System.Web.Http;
 
 namespace core_strength_yoga_products_api.Controllers
@@ -49,38 +50,56 @@ namespace core_strength_yoga_products_api.Controllers
             "FilterReport" +
             "/Username={username}" +
             "/StartDateTime={startDateTime}" +
-            "/EndDateTime={endDateTime}")]
+            "/EndDateTime={endDateTime}" +
+            "/ProductTypeId={productTypeId}")]
         public async Task<ActionResult<IEnumerable<StockAudit>>> FilterReport(
-            string username, 
-            string startDateTime, string endDateTime)
+            string startDateTime, string endDateTime, int productTypeId, string username)
         {
             var start = DateTime.Parse(startDateTime);
             var end = DateTime.Parse(endDateTime);
-            var usern = username.Replace("%20", " ");
-            
-            var stockAudits = _context.StockAudits.
-                Where(p => p.Username == usern && p.ChangedAt >= start && p.ChangedAt <= end).ToList();
+            var usern = "";
+            var prodTypeId = 0;
+            var productsByTypeId = _context.Products.SelectOnType(productTypeId);
 
+            var productIds = productsByTypeId.Select(p => p.Id).ToList();
+
+            var stockAuditsByProductTypeId = _context.StockAudits
+                .Where(s => productIds.Contains(s.ProductId));
+
+
+            if (username != "unknown")
+            {
+                usern = username.Replace("%20", " ");
+            }
+            if (productTypeId != 0 )
+            {
+                prodTypeId = productTypeId;
+            }
+
+            var stockAudits = new List<StockAudit>();
+            if (string.IsNullOrEmpty(usern) && prodTypeId == 0) 
+            {
+                stockAudits = _context.StockAudits.
+                    Where(p => p.ChangedAt >= start && p.ChangedAt <= end).ToList();
+            }
+            else if (string.IsNullOrEmpty(usern) && prodTypeId > 0)
+            {
+                stockAudits = stockAuditsByProductTypeId
+                    .Where(p => p.ChangedAt >= start && p.ChangedAt <= end).ToList();
+            }
+            else if (!string.IsNullOrEmpty(usern) && productTypeId == 0) 
+            {
+                stockAudits = _context.StockAudits
+                    .Where(p => p.Username == usern && p.ChangedAt >= start && p.ChangedAt <= end).ToList();
+            }
+            else if (!string.IsNullOrEmpty(usern) && prodTypeId > 0)
+            {
+                stockAudits = stockAuditsByProductTypeId
+                    .Where(p => p.Username == usern && p.ChangedAt >= start && p.ChangedAt <= end).ToList();
+            }
             if (stockAudits == null) return NotFound();
 
             return stockAudits.ToList();
         }
-        //public static List<Product> SelectOnProductType(this List<StockAudit> stockAudits, DbSet<StockAudit> set, int id)
-        //{
-        //    var ids = stockAudits
-        //         .Where(p => p.ProductId == id)
-        //         .Select(p => p.Id)
-        //         .ToList();
-
-        //    return id > 0 && stockAudits.Any() ? set
-        //     .IncludeAllRelated()
-        //     .Where(p => ids.Contains(p.Id))
-        //         .ToList() :
-        //     stockAudits;
-        //}
-
-        //[Microsoft.AspNetCore.Mvc.HttpGet("SearchByDatrange/{dateTime}")]
-
-
     }
 }
